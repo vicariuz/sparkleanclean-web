94635775e0ce71733711adc93419b3ff9d85e9d2
Adrian Arguello <adriangarguellom@yahoo.com>
2026-07-10 20:27:57 -0500

    perf: lazy loading routes + code splitting

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,30 @@
-import { StrictMode } from 'react'
+import { StrictMode, lazy, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
-import Blog from './Blog.jsx'
-import BlogPost from './BlogPost.jsx'
-import ResidentialCleaning from './ResidentialCleaning.jsx'
-import CommercialCleaning from './CommercialCleaning.jsx'
-import DeepCleaning from './DeepCleaning.jsx'
-import MoveOutCleaning from './MoveOutCleaning.jsx'
 
-
-
+const App = lazy(() => import('./App.jsx'))
+const Blog = lazy(() => import('./Blog.jsx'))
+const BlogPost = lazy(() => import('./BlogPost.jsx'))
+const ResidentialCleaning = lazy(() => import('./ResidentialCleaning.jsx'))
+const CommercialCleaning = lazy(() => import('./CommercialCleaning.jsx'))
+const DeepCleaning = lazy(() => import('./DeepCleaning.jsx'))
+const MoveOutCleaning = lazy(() => import('./MoveOutCleaning.jsx'))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/residential-cleaning-lafayette" element={<ResidentialCleaning />} />
-        <Route path="/commercial-cleaning-lafayette" element={<CommercialCleaning />} />
-        <Route path="/deep-cleaning-lafayette" element={<DeepCleaning />} />
-        <Route path="/move-out-cleaning-lafayette" element={<MoveOutCleaning />} />
-      </Routes>
+      <Suspense fallback={<div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/residential-cleaning-lafayette" element={<ResidentialCleaning />} />
+          <Route path="/commercial-cleaning-lafayette" element={<CommercialCleaning />} />
+          <Route path="/deep-cleaning-lafayette" element={<DeepCleaning />} />
+          <Route path="/move-out-cleaning-lafayette" element={<MoveOutCleaning />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </StrictMode>,
 )
